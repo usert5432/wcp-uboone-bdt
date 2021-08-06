72837518b9d42976fa82808b07480cccdb3d8e88
Xin Qian <last.qx@gmail.com>
2021-08-06 10:11:53 -0400

    update code

---
+++ apps/event_info_filter.cxx
@@ -0,0 +1,430 @@
+// cz: code modified from tutorials/tmva/TMVAClassification.C
+
+#include <cstdlib>
+#include <iostream>
+#include <map>
+#include <string>
+#include <set>
+
+#include "TChain.h"
+#include "TFile.h"
+#include "TTree.h"
+#include "TString.h"
+#include "TObjString.h"
+#include "TSystem.h"
+#include "TROOT.h"
+#include "TMath.h"
+
+#include "WCPLEEANA/tagger.h"
+
+#include "TMVA/Factory.h"
+#include "TMVA/DataLoader.h"
+#include "TMVA/Tools.h"
+#include "TMVA/TMVAGui.h"
+#include "TMVA/Reader.h"
+
+
+#include "WCPLEEANA/eval.h"
+
+using namespace std;
+using namespace LEEana;
+
+#include "WCPLEEANA/bdt.h"
+#include "WCPLEEANA/pot.h"
+#include "WCPLEEANA/pfeval.h"
+#include "WCPLEEANA/kine.h"
+#include "WCPLEEANA/cuts.h"
+
+int main( int argc, char** argv )
+{
+  if (argc < 2) {
+    std::cout << "event_filter #input_file #prefix_outfile " << std::endl;
+    return -1;
+  }
+
+  TString input_file = argv[1];
+  TString prefix_out = argv[2];
+  
+
+  TString outfile_event_name;
+  TString outfile_run_name;
+  
+
+  outfile_event_name = prefix_out + "_event_list.txt";
+  outfile_run_name = prefix_out + "_run.txt";
+  
+  
+
+
+  TFile *file1 = new TFile(input_file);
+  TTree *T_BDTvars = (TTree*)file1->Get("wcpselection/T_BDTvars");
+  
+  TTree *T_eval = (TTree*)file1->Get("wcpselection/T_eval");
+  TTree *T_pot = (TTree*)file1->Get("wcpselection/T_pot");
+  TTree *T_PFeval = (TTree*)file1->Get("wcpselection/T_PFeval");
+  TTree *T_KINEvars = (TTree*)file1->Get("wcpselection/T_KINEvars");
+
+
+  bool flag_data = true;
+  if (T_eval->GetBranch("weight_cv")) flag_data = false;
+  
+  EvalInfo eval;
+  eval.file_type = new std::string();
+  POTInfo pot;
+  TaggerInfo tagger;
+  PFevalInfo pfeval;
+  KineInfo kine;
+
+  kine.kine_energy_particle = new std::vector<float>;
+  kine.kine_energy_info = new std::vector<int>;
+  kine.kine_particle_type = new std::vector<int>;
+  kine.kine_energy_included = new std::vector<int>;
+    
+  tagger.pio_2_v_dis2 = new std::vector<float>;
+  tagger.pio_2_v_angle2 = new std::vector<float>;
+  tagger.pio_2_v_acc_length = new std::vector<float>;
+  tagger.pio_2_v_flag = new std::vector<float>;
+  tagger.sig_1_v_angle = new std::vector<float>;
+  tagger.sig_1_v_flag_single_shower = new std::vector<float>;
+  tagger.sig_1_v_energy = new std::vector<float>;
+  tagger.sig_1_v_energy_1 = new std::vector<float>;
+  tagger.sig_1_v_flag = new std::vector<float>;
+  tagger.sig_2_v_energy = new std::vector<float>;
+  tagger.sig_2_v_shower_angle = new std::vector<float>;
+  tagger.sig_2_v_flag_single_shower = new std::vector<float>;
+  tagger.sig_2_v_medium_dQ_dx = new std::vector<float>;
+  tagger.sig_2_v_start_dQ_dx = new std::vector<float>;
+  tagger.sig_2_v_flag = new std::vector<float>;
+  tagger.stw_2_v_medium_dQ_dx = new std::vector<float>;
+  tagger.stw_2_v_energy = new std::vector<float>;
+  tagger.stw_2_v_angle = new std::vector<float>;
+  tagger.stw_2_v_dir_length = new std::vector<float>;
+  tagger.stw_2_v_max_dQ_dx = new std::vector<float>;
+  tagger.stw_2_v_flag = new std::vector<float>;
+  tagger.stw_3_v_angle = new std::vector<float>;
+  tagger.stw_3_v_dir_length = new std::vector<float>;
+  tagger.stw_3_v_energy = new std::vector<float>;
+  tagger.stw_3_v_medium_dQ_dx = new std::vector<float>;
+  tagger.stw_3_v_flag = new std::vector<float>;
+  tagger.stw_4_v_angle = new std::vector<float>;
+  tagger.stw_4_v_dis = new std::vector<float>;
+  tagger.stw_4_v_energy = new std::vector<float>;
+  tagger.stw_4_v_flag = new std::vector<float>;
+  tagger.br3_3_v_energy = new std::vector<float>;
+  tagger.br3_3_v_angle = new std::vector<float>;
+  tagger.br3_3_v_dir_length = new std::vector<float>;
+  tagger.br3_3_v_length = new std::vector<float>;
+  tagger.br3_3_v_flag = new std::vector<float>;
+  tagger.br3_5_v_dir_length = new std::vector<float>;
+  tagger.br3_5_v_total_length = new std::vector<float>;
+  tagger.br3_5_v_flag_avoid_muon_check = new std::vector<float>;
+  tagger.br3_5_v_n_seg = new std::vector<float>;
+  tagger.br3_5_v_angle = new std::vector<float>;
+  tagger.br3_5_v_sg_length = new std::vector<float>;
+  tagger.br3_5_v_energy = new std::vector<float>;
+  tagger.br3_5_v_n_main_segs = new std::vector<float>;
+  tagger.br3_5_v_n_segs = new std::vector<float>;
+  tagger.br3_5_v_shower_main_length = new std::vector<float>;
+  tagger.br3_5_v_shower_total_length = new std::vector<float>;
+  tagger.br3_5_v_flag = new std::vector<float>;
+  tagger.br3_6_v_angle = new std::vector<float>;
+  tagger.br3_6_v_angle1 = new std::vector<float>;
+  tagger.br3_6_v_flag_shower_trajectory = new std::vector<float>;
+  tagger.br3_6_v_direct_length = new std::vector<float>;
+  tagger.br3_6_v_length = new std::vector<float>;
+  tagger.br3_6_v_n_other_vtx_segs = new std::vector<float>;
+  tagger.br3_6_v_energy = new std::vector<float>;
+  tagger.br3_6_v_flag = new std::vector<float>;
+  tagger.tro_1_v_particle_type = new std::vector<float>;
+  tagger.tro_1_v_flag_dir_weak = new std::vector<float>;
+  tagger.tro_1_v_min_dis = new std::vector<float>;
+  tagger.tro_1_v_sg1_length = new std::vector<float>;
+  tagger.tro_1_v_shower_main_length = new std::vector<float>;
+  tagger.tro_1_v_max_n_vtx_segs = new std::vector<float>;
+  tagger.tro_1_v_tmp_length = new std::vector<float>;
+  tagger.tro_1_v_medium_dQ_dx = new std::vector<float>;
+  tagger.tro_1_v_dQ_dx_cut = new std::vector<float>;
+  tagger.tro_1_v_flag_shower_topology = new std::vector<float>;
+  tagger.tro_1_v_flag = new std::vector<float>;
+  tagger.tro_2_v_energy = new std::vector<float>;
+  tagger.tro_2_v_stem_length = new std::vector<float>;
+  tagger.tro_2_v_iso_angle = new std::vector<float>;
+  tagger.tro_2_v_max_length = new std::vector<float>;
+  tagger.tro_2_v_angle = new std::vector<float>;
+  tagger.tro_2_v_flag = new std::vector<float>;
+  tagger.tro_4_v_dir2_mag = new std::vector<float>;
+  tagger.tro_4_v_angle = new std::vector<float>;
+  tagger.tro_4_v_angle1 = new std::vector<float>;
+  tagger.tro_4_v_angle2 = new std::vector<float>;
+  tagger.tro_4_v_length = new std::vector<float>;
+  tagger.tro_4_v_length1 = new std::vector<float>;
+  tagger.tro_4_v_medium_dQ_dx = new std::vector<float>;
+  tagger.tro_4_v_end_dQ_dx = new std::vector<float>;
+  tagger.tro_4_v_energy = new std::vector<float>;
+  tagger.tro_4_v_shower_main_length = new std::vector<float>;
+  tagger.tro_4_v_flag_shower_trajectory = new std::vector<float>;
+  tagger.tro_4_v_flag = new std::vector<float>;
+  tagger.tro_5_v_max_angle = new std::vector<float>;
+  tagger.tro_5_v_min_angle = new std::vector<float>;
+  tagger.tro_5_v_max_length = new std::vector<float>;
+  tagger.tro_5_v_iso_angle = new std::vector<float>;
+  tagger.tro_5_v_n_vtx_segs = new std::vector<float>;
+  tagger.tro_5_v_min_count = new std::vector<float>;
+  tagger.tro_5_v_max_count = new std::vector<float>;
+  tagger.tro_5_v_energy = new std::vector<float>;
+  tagger.tro_5_v_flag = new std::vector<float>;
+  tagger.lol_1_v_energy = new std::vector<float>;
+  tagger.lol_1_v_vtx_n_segs = new std::vector<float>;
+  tagger.lol_1_v_nseg = new std::vector<float>;
+  tagger.lol_1_v_angle = new std::vector<float>;
+  tagger.lol_1_v_flag = new std::vector<float>;
+  tagger.lol_2_v_length = new std::vector<float>;
+  tagger.lol_2_v_angle = new std::vector<float>;
+  tagger.lol_2_v_type = new std::vector<float>;
+  tagger.lol_2_v_vtx_n_segs = new std::vector<float>;
+  tagger.lol_2_v_energy = new std::vector<float>;
+  tagger.lol_2_v_shower_main_length = new std::vector<float>;
+  tagger.lol_2_v_flag_dir_weak = new std::vector<float>;
+  tagger.lol_2_v_flag = new std::vector<float>;
+  tagger.cosmict_flag_10 = new std::vector<float>;
+  tagger.cosmict_10_flag_inside = new std::vector<float>;
+  tagger.cosmict_10_vtx_z = new std::vector<float>;
+  tagger.cosmict_10_flag_shower = new std::vector<float>;
+  tagger.cosmict_10_flag_dir_weak = new std::vector<float>;
+  tagger.cosmict_10_angle_beam = new std::vector<float>;
+  tagger.cosmict_10_length = new std::vector<float>;
+  tagger.numu_cc_flag_1 = new std::vector<float>;
+  tagger.numu_cc_1_particle_type = new std::vector<float>;
+  tagger.numu_cc_1_length = new std::vector<float>;
+  tagger.numu_cc_1_medium_dQ_dx = new std::vector<float>;
+  tagger.numu_cc_1_dQ_dx_cut = new std::vector<float>;
+  tagger.numu_cc_1_direct_length = new std::vector<float>;
+  tagger.numu_cc_1_n_daughter_tracks = new std::vector<float>;
+  tagger.numu_cc_1_n_daughter_all = new std::vector<float>;
+  tagger.numu_cc_flag_2 = new std::vector<float>;
+  tagger.numu_cc_2_length = new std::vector<float>;
+  tagger.numu_cc_2_total_length = new std::vector<float>;
+  tagger.numu_cc_2_n_daughter_tracks = new std::vector<float>;
+  tagger.numu_cc_2_n_daughter_all = new std::vector<float>;
+  tagger.pio_2_v_dis2 = new std::vector<float>;
+  tagger.pio_2_v_angle2 = new std::vector<float>;
+  tagger.pio_2_v_acc_length = new std::vector<float>;
+  tagger.pio_2_v_flag = new std::vector<float>;
+  tagger.sig_1_v_angle = new std::vector<float>;
+  tagger.sig_1_v_flag_single_shower = new std::vector<float>;
+  tagger.sig_1_v_energy = new std::vector<float>;
+  tagger.sig_1_v_energy_1 = new std::vector<float>;
+  tagger.sig_1_v_flag = new std::vector<float>;
+  tagger.sig_2_v_energy = new std::vector<float>;
+  tagger.sig_2_v_shower_angle = new std::vector<float>;
+  tagger.sig_2_v_flag_single_shower = new std::vector<float>;
+  tagger.sig_2_v_medium_dQ_dx = new std::vector<float>;
+  tagger.sig_2_v_start_dQ_dx = new std::vector<float>;
+  tagger.sig_2_v_flag = new std::vector<float>;
+  tagger.stw_2_v_medium_dQ_dx = new std::vector<float>;
+  tagger.stw_2_v_energy = new std::vector<float>;
+  tagger.stw_2_v_angle = new std::vector<float>;
+  tagger.stw_2_v_dir_length = new std::vector<float>;
+  tagger.stw_2_v_max_dQ_dx = new std::vector<float>;
+  tagger.stw_2_v_flag = new std::vector<float>;
+  tagger.stw_3_v_angle = new std::vector<float>;
+  tagger.stw_3_v_dir_length = new std::vector<float>;
+  tagger.stw_3_v_energy = new std::vector<float>;
+  tagger.stw_3_v_medium_dQ_dx = new std::vector<float>;
+  tagger.stw_3_v_flag = new std::vector<float>;
+  tagger.stw_4_v_angle = new std::vector<float>;
+  tagger.stw_4_v_dis = new std::vector<float>;
+  tagger.stw_4_v_energy = new std::vector<float>;
+  tagger.stw_4_v_flag = new std::vector<float>;
+  tagger.br3_3_v_energy = new std::vector<float>;
+  tagger.br3_3_v_angle = new std::vector<float>;
+  tagger.br3_3_v_dir_length = new std::vector<float>;
+  tagger.br3_3_v_length = new std::vector<float>;
+  tagger.br3_3_v_flag = new std::vector<float>;
+  tagger.br3_5_v_dir_length = new std::vector<float>;
+  tagger.br3_5_v_total_length = new std::vector<float>;
+  tagger.br3_5_v_flag_avoid_muon_check = new std::vector<float>;
+  tagger.br3_5_v_n_seg = new std::vector<float>;
+  tagger.br3_5_v_angle = new std::vector<float>;
+  tagger.br3_5_v_sg_length = new std::vector<float>;
+  tagger.br3_5_v_energy = new std::vector<float>;
+  tagger.br3_5_v_n_main_segs = new std::vector<float>;
+  tagger.br3_5_v_n_segs = new std::vector<float>;
+  tagger.br3_5_v_shower_main_length = new std::vector<float>;
+  tagger.br3_5_v_shower_total_length = new std::vector<float>;
+  tagger.br3_5_v_flag = new std::vector<float>;
+  tagger.br3_6_v_angle = new std::vector<float>;
+  tagger.br3_6_v_angle1 = new std::vector<float>;
+  tagger.br3_6_v_flag_shower_trajectory = new std::vector<float>;
+  tagger.br3_6_v_direct_length = new std::vector<float>;
+  tagger.br3_6_v_length = new std::vector<float>;
+  tagger.br3_6_v_n_other_vtx_segs = new std::vector<float>;
+  tagger.br3_6_v_energy = new std::vector<float>;
+  tagger.br3_6_v_flag = new std::vector<float>;
+  tagger.tro_1_v_particle_type = new std::vector<float>;
+  tagger.tro_1_v_flag_dir_weak = new std::vector<float>;
+  tagger.tro_1_v_min_dis = new std::vector<float>;
+  tagger.tro_1_v_sg1_length = new std::vector<float>;
+  tagger.tro_1_v_shower_main_length = new std::vector<float>;
+  tagger.tro_1_v_max_n_vtx_segs = new std::vector<float>;
+  tagger.tro_1_v_tmp_length = new std::vector<float>;
+  tagger.tro_1_v_medium_dQ_dx = new std::vector<float>;
+  tagger.tro_1_v_dQ_dx_cut = new std::vector<float>;
+  tagger.tro_1_v_flag_shower_topology = new std::vector<float>;
+  tagger.tro_1_v_flag = new std::vector<float>;
+  tagger.tro_2_v_energy = new std::vector<float>;
+  tagger.tro_2_v_stem_length = new std::vector<float>;
+  tagger.tro_2_v_iso_angle = new std::vector<float>;
+  tagger.tro_2_v_max_length = new std::vector<float>;
+  tagger.tro_2_v_angle = new std::vector<float>;
+  tagger.tro_2_v_flag = new std::vector<float>;
+  tagger.tro_4_v_dir2_mag = new std::vector<float>;
+  tagger.tro_4_v_angle = new std::vector<float>;
+  tagger.tro_4_v_angle1 = new std::vector<float>;
+  tagger.tro_4_v_angle2 = new std::vector<float>;
+  tagger.tro_4_v_length = new std::vector<float>;
+  tagger.tro_4_v_length1 = new std::vector<float>;
+  tagger.tro_4_v_medium_dQ_dx = new std::vector<float>;
+  tagger.tro_4_v_end_dQ_dx = new std::vector<float>;
+  tagger.tro_4_v_energy = new std::vector<float>;
+  tagger.tro_4_v_shower_main_length = new std::vector<float>;
+  tagger.tro_4_v_flag_shower_trajectory = new std::vector<float>;
+  tagger.tro_4_v_flag = new std::vector<float>;
+  tagger.tro_5_v_max_angle = new std::vector<float>;
+  tagger.tro_5_v_min_angle = new std::vector<float>;
+  tagger.tro_5_v_max_length = new std::vector<float>;
+  tagger.tro_5_v_iso_angle = new std::vector<float>;
+  tagger.tro_5_v_n_vtx_segs = new std::vector<float>;
+  tagger.tro_5_v_min_count = new std::vector<float>;
+  tagger.tro_5_v_max_count = new std::vector<float>;
+  tagger.tro_5_v_energy = new std::vector<float>;
+  tagger.tro_5_v_flag = new std::vector<float>;
+  tagger.lol_1_v_energy = new std::vector<float>;
+  tagger.lol_1_v_vtx_n_segs = new std::vector<float>;
+  tagger.lol_1_v_nseg = new std::vector<float>;
+  tagger.lol_1_v_angle = new std::vector<float>;
+  tagger.lol_1_v_flag = new std::vector<float>;
+  tagger.lol_2_v_length = new std::vector<float>;
+  tagger.lol_2_v_angle = new std::vector<float>;
+  tagger.lol_2_v_type = new std::vector<float>;
+  tagger.lol_2_v_vtx_n_segs = new std::vector<float>;
+  tagger.lol_2_v_energy = new std::vector<float>;
+  tagger.lol_2_v_shower_main_length = new std::vector<float>;
+  tagger.lol_2_v_flag_dir_weak = new std::vector<float>;
+  tagger.lol_2_v_flag = new std::vector<float>;
+  tagger.cosmict_flag_10 = new std::vector<float>;
+  tagger.cosmict_10_flag_inside = new std::vector<float>;
+  tagger.cosmict_10_vtx_z = new std::vector<float>;
+  tagger.cosmict_10_flag_shower = new std::vector<float>;
+  tagger.cosmict_10_flag_dir_weak = new std::vector<float>;
+  tagger.cosmict_10_angle_beam = new std::vector<float>;
+  tagger.cosmict_10_length = new std::vector<float>;
+  tagger.numu_cc_flag_1 = new std::vector<float>;
+  tagger.numu_cc_1_particle_type = new std::vector<float>;
+  tagger.numu_cc_1_length = new std::vector<float>;
+  tagger.numu_cc_1_medium_dQ_dx = new std::vector<float>;
+  tagger.numu_cc_1_dQ_dx_cut = new std::vector<float>;
+  tagger.numu_cc_1_direct_length = new std::vector<float>;
+  tagger.numu_cc_1_n_daughter_tracks = new std::vector<float>;
+  tagger.numu_cc_1_n_daughter_all = new std::vector<float>;
+  tagger.numu_cc_flag_2 = new std::vector<float>;
+  tagger.numu_cc_2_length = new std::vector<float>;
+  tagger.numu_cc_2_total_length = new std::vector<float>;
+  tagger.numu_cc_2_n_daughter_tracks = new std::vector<float>;
+  tagger.numu_cc_2_n_daughter_all = new std::vector<float>;
+
+  set_tree_address(T_BDTvars, tagger,2 );
+
+  if (flag_data){
+    set_tree_address(T_eval, eval,2);
+
+    set_tree_address(T_PFeval, pfeval,2);
+
+  }else{
+    set_tree_address(T_eval, eval);
+
+    set_tree_address(T_PFeval, pfeval);
+  }
+
+  set_tree_address(T_pot, pot);
+
+
+  set_tree_address(T_KINEvars, kine);
+    
+
+  T_eval->SetBranchStatus("*",1);
+  T_BDTvars->SetBranchStatus("*",1);
+
+  
+  ofstream outfile1(outfile_event_name);
+  
+  for (int i=0;i!=T_BDTvars->GetEntries();i++){
+    T_BDTvars->GetEntry(i);
+    T_eval->GetEntry(i); tagger.match_isFC = eval.match_isFC;
+    T_KINEvars->GetEntry(i); tagger.kine_reco_Enu = kine.kine_reco_Enu;
+    T_PFeval->GetEntry(i);
+  
+    
+    bool flag_far_sideband = LEEana::is_far_sideband(kine, tagger, flag_data);
+    bool flag_near_sideband = LEEana::is_near_sideband(kine,tagger, flag_data);
+    bool flag_LEE_signal = LEEana::is_LEE_signal(kine,tagger, flag_data);
+
+    bool flag_numuCC = LEEana::is_numuCC(tagger);
+    bool flag_pi0 = LEEana::is_pi0(kine, flag_data);
+    bool flag_NC = LEEana::is_NC(tagger);
+    bool flag_nueCC = LEEana::is_nueCC(tagger);
+    bool flag_0p = LEEana::is_0p(tagger, kine, pfeval);
+    bool flag_0pi = LEEana::is_0pi(tagger,kine, pfeval);
+    //   if (filter_level==1){
+    //     // numuCC filter
+    //     if (flag_far_sideband && (flag_numuCC || flag_pi0 && flag_NC) || tagger.numu_cc_flag==-1)
+    // 	flag_remove = false;
+    //     if (flag_near_sideband || flag_LEE_signal) continue;
+    //   }else if (filter_level == 2){
+    //     // far sideband
+    //     if (flag_far_sideband || tagger.numu_cc_flag==-1)
+    // 	flag_remove = false;
+    //     if (flag_near_sideband || flag_LEE_signal) continue;
+    //   }else if (filter_level == 3){
+    //     // near sideband
+    //     if (flag_far_sideband || flag_near_sideband || tagger.numu_cc_flag==-1)
+    // 	flag_remove = false;
+    //     if (flag_LEE_signal) continue;
+    //   }else if (filter_level == 4){
+    //     // signal region  
+    // 	flag_remove = false;
+    //   }
+
+    double pio_energy = 0;
+    if (flag_pi0)
+      pio_energy = LEEana::get_kine_var(kine, eval, pfeval, tagger, flag_data, "pi0_energy");
+    
+    if (flag_far_sideband || flag_near_sideband || flag_LEE_signal){
+      outfile1 << " " << eval.run << " " << eval.subrun << " " << eval.event << " " // event
+	       << LEEana::get_reco_Enu_corr(kine, flag_data) << " " << pfeval.reco_nuvtxX << " " << pfeval.reco_nuvtxY << " " << pfeval.reco_nuvtxZ << " " << eval.match_isFC << " " // general neutrino information
+	       << flag_numuCC << " " << tagger.numu_score << " " // numuCC information
+	       << flag_pi0 << " " << flag_NC << " " << pio_energy << " "// pi0 information
+	       << flag_nueCC << " " << tagger.nue_score << " " // nueCC information
+	       << flag_0p << " " << flag_0pi << " " // 0p or Np
+	       << std::endl;
+    }
+  }
+
+
+  ofstream outfile2(outfile_run_name);
+  for (Int_t i=0;i!=T_pot->GetEntries();i++){
+    T_pot->GetEntry(i);
+    outfile2 << pot.runNo << " " << pot.subRunNo << std::endl;
+  }
+
+
+  
+
+  //  std::cout << flag_data << std::endl;
+
+
+  return 0;
+
+  
+  
+}
--- inc/WCPLEEANA/cuts.h
+++ inc/WCPLEEANA/cuts.h
@@ -55,6 +55,7 @@ namespace LEEana{
   bool is_numuCC_tight(TaggerInfo& tagger_info, PFevalInfo& pfeval);
   bool is_numuCC_1mu0p(TaggerInfo& tagger_info, KineInfo& kine, PFevalInfo& pfeval);
   bool is_0p(TaggerInfo& tagger_info, KineInfo& kine, PFevalInfo& pfeval);
+  bool is_0pi(TaggerInfo& tagger_info, KineInfo& kine, PFevalInfo& pfeval);
   bool is_numuCC_lowEhad(TaggerInfo& tagger_info, KineInfo& kine, PFevalInfo& pfeval, bool flag_data);
   bool is_numuCC_cutbased(TaggerInfo& tagger_info);
   
@@ -2071,6 +2072,30 @@ bool LEEana::is_0p(TaggerInfo& tagger_info, KineInfo& kine, PFevalInfo& pfeval){
   
   return flag;
 }
+
+
+bool LEEana::is_0pi(TaggerInfo& tagger_info, KineInfo& kine, PFevalInfo& pfeval){
+  bool flag = false;
+  
+  if (tagger_info.numu_cc_flag>=0){ 
+      // 1 lepton <=1 proton 0 charged pion
+      // 1 lepton guaranteed by numu cc flag
+      // using pi0 flag to remove pi0 component in channel definition
+      int Nproton = 0;
+      int Npion = 0;
+      for(size_t i=0; i<kine.kine_energy_particle->size(); i++)
+      {
+          int pdgcode = kine.kine_particle_type->at(i);
+          if(abs(pdgcode)==2212 && kine.kine_energy_particle->at(i)>35) Nproton++; // KE threshold: 50 MeV, 1.5 cm? 
+          if(abs(pdgcode)==211 && kine.kine_energy_particle->at(i)>10) Npion++; // KE threshold: 10 MeV 
+      }
+      if(Npion==0) flag = true;
+  } 
+  
+  return flag;
+}
+
+
 
 bool LEEana::is_numuCC_1mu0p(TaggerInfo& tagger_info, KineInfo& kine, PFevalInfo& pfeval){
   bool flag = false;
